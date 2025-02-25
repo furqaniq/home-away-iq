4c1382f54d24e6017360d48ad30d2bcebeb200d5
furqaniq <furqan93@gmail.com>
2025-02-25 17:50:35 +0500

    RatingInput added

---
+++ components/reviews/PropertyReviews.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PropertyReviews() {
+  return (
+    <div>
+      PropertyReviews
+    </div>
+  )
+}
+
+export default PropertyReviews
---
+++ components/reviews/ReviewCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ReviewCard() {
+  return (
+    <div>
+      ReviewCard
+    </div>
+  )
+}
+
+export default ReviewCard
